a405b0f628b5c1cdcf097b1cb9da6ca25cf7771a
Colin T. Riley <riley.ct.13@gmail.com>
2017-04-11 22:54:25 -0600

    Added Manipulation, just needs scale?
    Manipulation added for the most part I think.  I followed a tutorial to get to this point.  Not sure how it plays with selection.  Scaling not implemented.

---
+++ project 2/Assets/Manipulation.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manipulation : MonoBehaviour {
+	private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+    public bool gripButtonDown = false;
+    public bool gripButtonUp = false;
+    public bool gripButtonPressed = false;
+
+
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+    public bool triggerButtonDown = false;
+    public bool triggerButtonUp = false;
+    public bool triggerButtonPressed = false;
+  
+    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
+    private SteamVR_TrackedObject trackedObj;
+  
+    private GameObject pickup;
+
+  	// Use this for initialization
+  	void Start () {
+          trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+  	void Update() {
+  		if(controller == null){
+  			Debug.Log("Controller not initialiezed");
+  			return;
+  		}
+
+        if (controller.GetPressDown(gripButton) && pickup != null) {
+            pickup.transform.parent = this.transform;
+            pickup.GetComponent<Rigidbody>().isKinematic = true;
+          }
+        if (controller.GetPressUp(gripButton) && pickup != null) {
+           pickup.transform.parent = null;
+            pickup.GetComponent<Rigidbody>().isKinematic = true;
+          }
+  	}
+
+    private void OnTriggerEnter(Collider collider) {
+        pickup = collider.gameObject;
+    }
+
+    private void OnTriggerExit(Collider collider) {
+        pickup = null;
+    }
+  }
