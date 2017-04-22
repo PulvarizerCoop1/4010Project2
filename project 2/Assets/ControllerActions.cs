d0f6b5e71df171d87a8d31bd1106a1a15b8ba69f
Colin T. Riley <riley.ct.13@gmail.com>
2017-04-21 18:39:24 -0600

    Commented and instructions, carts fixed
    Should be final upload.  Code is totally commented and instrucitons are include, as are the who did what, etc

--- project 2/Assets/ControllerActions.cs
+++ project 2/Assets/ControllerActions.cs
@@ -1,19 +1,31 @@
 ﻿/*
+ * Adam Kacmarsky, Jordan Cooper, Colin Riley
  * 
- * Code was created with help from
- * https://www.raywenderlich.com/149239/htc-vive-tutorial-unity
+ * Overall:
+ * 	Colin developed the environment and helped with selection/manipulation
+ * 	Jordan did the selection/manipulation
+ * 	Adam worked on navigation
+ * However most of the work was done in lab and together.
  * 
+ * Some code inspiration for how to implement selection and manipulation taken from
+ * 		https://www.raywenderlich.com/149239/htc-vive-tutorial-unity
+ * 
+ * Instructions
+ * Use the grip button on the ccontroller to pick up and object and move the controller to manipulate it.
+ * When an object is in hand, click the right side of the touch pad to increase the size of it.  Click the
+ * 		left side of the touch pad to scale the object down.
+ * Press the trigger down to set the location of the teleport and release it to go to that location.
  */
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ControllerGrabObject : MonoBehaviour
+public class ControllerActions : MonoBehaviour
 {
 
     // Use this for initialization
-    private SteamVR_TrackedObject trackedObj;
+    private SteamVR_TrackedObject trackedObject;
 
     // Store the GameObject that the trigger is currently colliding with,
     //  so we can grab the object
@@ -22,16 +34,21 @@ public class ControllerGrabObject : MonoBehaviour
     //  grabbing
     private GameObject objectInHand;
 
+    // cube object to make teleport location
 	private	GameObject cube;
+
+	// bool for limiting teleportation, untested
+	//private bool canTP;
 
     private SteamVR_Controller.Device Controller
     {
-        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+        get { return SteamVR_Controller.Input((int)trackedObject.index); }
     }
 
     void Awake()
     {
-        trackedObj = GetComponent<SteamVR_TrackedObject>();
+        trackedObject = GetComponent<SteamVR_TrackedObject>();
+        //canTP = true;
     }
 
     // Accepts Collider as parameter and uses its GameObject as the collidingObject
@@ -75,7 +92,7 @@ public class ControllerGrabObject : MonoBehaviour
         collidingObject = null;
     }
 
-    private void GrabObject()
+    private void SelectObject()
     {
         // Move the GameObject inside the player's hand and remove it from
         //  the collidingObject variable
@@ -98,7 +115,7 @@ public class ControllerGrabObject : MonoBehaviour
     }
 
     // Handles releasing the object
-    private void ReleaseObject()
+    private void DeselectObject()
     {
         // Check if fixed joint is attached to the controller
         if (GetComponent<FixedJoint>())
@@ -115,65 +132,82 @@ public class ControllerGrabObject : MonoBehaviour
         objectInHand = null;
     }
 
-	void ScaleUp(){
+	// scales object size up
+    void ScaleUp(){
 		objectInHand.transform.localScale += new Vector3 (.5f, .5f, .5f);
-		//objectInHand.GetComponent<BoxCollider>().size += new Vector3 (.1f, .1f, .1f);
+		//objectInHand.GetComponent<BoxCollider>().size += new Vector3 (.1f, .1f, .1f); // can be used to scale the box collieder, removed because couldn't figure the proper scale
 	}
 
+    // scales object size down
 	void ScaleDown(){
 		objectInHand.transform.localScale -= new Vector3 (.5f, .5f, .5f);
-		//objectInHand.GetComponent<BoxCollider>().size -= new Vector3 (.1f, .1f, .1f);
+		//objectInHand.GetComponent<BoxCollider>().size -= new Vector3 (.1f, .1f, .1f); // can be used to scale the box collieder, removed because couldn't figure the proper scale
 	}
 
     // Update is called once per frame
     void Update()
 	{
+    	// on grip press, select obejct to manipulate
 		if (Controller.GetPressDown(Valve.VR.EVRButtonId.k_EButton_Grip))
 		{
 			if (collidingObject)
 			{
-				GrabObject();
+				SelectObject();
 			}
 		}
 
+		// let go of object when grip is released
 		if (Controller.GetPressUp(Valve.VR.EVRButtonId.k_EButton_Grip))
 		{
 			if (objectInHand)
 			{
-				ReleaseObject();
+				DeselectObject();
 			}
 		}
 
+		// on touch pad click right, scale held object up in size
 		if (Controller.GetAxis (Valve.VR.EVRButtonId.k_EButton_Axis0) [0] > 0.8f
 		   && Controller.GetPressDown (Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad)) {
-			print ("here1");
 			ScaleUp();
 		}
 
+		// on touch pad click left, scale held object down in size
 		if (Controller.GetAxis (Valve.VR.EVRButtonId.k_EButton_Axis0) [0] < 0.8f
 			&& Controller.GetPressDown (Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad)) {
 			print ("here2");
 			ScaleDown();
 		}
 
-		if (Controller.GetHairTriggerDown ()) {
+		// on trigger press, aim teleport
+		if (Controller.GetHairTriggerDown () ) {	//&& canTP
+			// creates vectors for moving position
 			Vector3 startPos = transform.position;
 			Vector3 endPos = startPos + transform.forward * 4f;
 
+			// creates object for cube to mark teleport location
 			cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
 
+			// cube's scale and location
 			cube.transform.position = new Vector3(endPos[0],.5f,endPos[2]);
 			cube.transform.localScale = new Vector3 (.5f, .1f, .5f);
 
+			// removes collision from cube
 			Destroy (cube.GetComponent<Rigidbody> ());
 			Destroy (cube.GetComponent<BoxCollider> ());
 
+			// sets cubes color
 			cube.GetComponent<Renderer> ().material.color = Color.green;
+			
+			//canTP=false;
 		}
 
+		// on trigger release teleport
 		if (Controller.GetHairTriggerUp ()) {
+			// move camera, delete cube
 			transform.parent.position = cube.transform.position;
 			Destroy (cube);
+			
+			//canTP=true;
 		}
     }
 }
